a2899342fcaa50f8c389ed58234ef6a2fc26e8dd
andres0772 <andresesteb0772@gmail.com>
2025-10-28 15:24:33 -0500

    feat(init) se completaron todos los TODO

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,24 @@ Paquete principal de la aplicación API de Películas.
 Este módulo inicializa el paquete y expone los componentes principales.
 """
 
-# TODO: Importar los componentes principales para facilitar su uso
+# Importar los componentes principales para facilitar su uso
 # Ejemplo:
-# from .database import get_session, engine
-# from .models import Usuario, Pelicula, Favorito
-# from .config import settings
+from .database import get_session, engine
+from .models import Usuario, Pelicula, Favorito
+from .config import settings
 
 __version__ = "1.0.0"
-__author__ = "Tu Nombre"  # TODO: Reemplazar con tu nombre
+__author__ = "andres"  # Reemplazar con tu nombre
 
-# TODO: Opcional - Definir __all__ para controlar qué se exporta
-# __all__ = [
-# ]
+# Definir __all__ para controlar qué se exporta
+__all__ = [
+    "get_session",
+    "engine",
+    "Usuario",
+    "Pelicula",
+    "Favorito",
+    "settings",
+    "__version__",
+    "__author__"
+]
 
